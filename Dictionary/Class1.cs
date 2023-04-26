0f96c2c5682ad2f2eed5640cda98a9a239471e7b
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-04-26 21:49:27 +0900

    초기 커밋.

---
+++ Dictionary/Class1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class HashTable<TKey, TValue> where TKey : IEquatable<TKey>
+    {
+        private const int DefaultCapacity = 1000;
+        // 해시테이블의 크기를 1000으로 설정
+
+        private struct Entry
+        // 구조체 엔트리를 만듬
+        {
+            public enum State { None, Using, Deleted }
+            // State의 열거형을 만듬
+
+            public int hashCode;
+            public State state;
+            public TKey key;
+            public TValue value;
+        }
+
+        private Func<TKey, int> hashFunc;
+        private Entry[] table;
+
+        public HashTable()
+        {
+            table = new Entry[DefaultCapacity];
+            hashFunc = HashFunc;
+        }
+
+        public HashTable(Func<TKey, int> hashFunc)
+        {
+            this.table = new Entry[DefaultCapacity];
+            this.hashFunc = hashFunc;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                TValue value;
+                if (TryGetValue(key, out value))
+                    return value;
+                // key의 키 값에 value 데이터가 있을때 value의 데이터를 반환
+                else
+                    throw new KeyNotFoundException();
+                // key의 키 값에 value가 없을 시 예외처리함
+            }
+            set
+            {
+                TryInsert(key, value, InsertionBehavior.OverrideExist);
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TryInsert(key, value, InsertionBehavior.ThrowOnExisting);
+        }
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            return TryInsert(key, value, InsertionBehavior.None);
+        }
+
+        public void Clear()
+        {
+            table = new Entry[DefaultCapacity];
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return TryGetValue(key, out var value);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            int index = FindIndex(key);
+
+            if (index < 0)
+            {
+                value = default(TValue);
+                return false;
+            }
+            else
+            {
+                value = table[index].value;
+                return true;
+            }
+        }
+
+        public bool Remove(TKey key)
+        {
+            int index = FindIndex(key);
+
+            if (index < 0)
+            {
+                return false;
+            }
+            else
+            {
+                table[index].state = Entry.State.Deleted;
+                return true;
+            }
+        }
+
+        private int HashFunc(TKey key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+
+            return key.GetHashCode();
+        }
+
+        private int DoubleHash(int index)
+        {
+            return ++index % table.Length;
+        }
+
+        private enum InsertionBehavior { None, OverrideExist, ThrowOnExisting }
+        private bool TryInsert(TKey key, TValue value, InsertionBehavior behavior)
+        {
+            int hashCode = hashFunc(key);
+            int index = Math.Abs(hashCode) % table.Length;
+            while (table[index].state == Entry.State.Using)
+            {
+                if (key.Equals(table[index].key))
+                {
+                    switch (behavior)
+                    {
+                        case InsertionBehavior.OverrideExist:
+                            table[index].hashCode = hashCode;
+                            table[index].key = key;
+                            table[index].value = value;
+                            return true;
+                        case InsertionBehavior.ThrowOnExisting:
+                            throw new ArgumentException();
+                        case InsertionBehavior.None:
+                        default:
+                            return false;
+                    }
+                }
+                index = DoubleHash(index);
+            }
+
+            table[index].hashCode = hashCode;
+            table[index].state = Entry.State.Using;
+            table[index].key = key;
+            table[index].value = value;
+            return true;
+        }
+
+        private int FindIndex(TKey key)
+        {
+            int hashCode = hashFunc(key);
+            int index = Math.Abs(hashCode) % table.Length;
+            while (table[index].state == Entry.State.Using)
+            {
+                if (key.Equals(table[index].key))
+                {
+                    return index;
+                }
+                index = DoubleHash(index);
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        void Dictionary()
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            // 키와 데이터를 해싱함                                               //
+                                                                                  //
+            dictionary.Add("txt", "텍스트 파일");                                 // 
+            // "txt" : key, "텍스트 파일" : 데이터                                //
+            dictionary.Add("bmp", "이미지 파일");                                 //
+            dictionary.Add("mp3", "오디오 파일");                                 //
+                                                                                  //
+            Console.WriteLine(dictionary["txt"]);                                 //
+            // 인덱서를 통해 키값에 접근                                          //
+            if (dictionary.ContainsKey("mp3"))                                    //
+                Console.WriteLine("mp3 키 값의 데이터가 있음");                   //
+            // mp3 키 값이 있을 때                                                //
+            else                                                                  //
+                Console.WriteLine("mp3 키 값의 데이터가 없음");                   //
+            // mp3 키 값이 없을 때                                                //
+                                                                                  //
+            if (dictionary.Remove("mp3"))                                         //
+                Console.WriteLine("mp3 키 값에 해당하는 데이터를 지움");          //
+            // mp3 키 값이 있을 때 삭제함                                         //
+            else                                                                  //
+                Console.WriteLine("mp3 키 값에 해당하는 데이터를 못지움");        //
+            // mp3 키 값이 없어서 데이터를 삭제하지 못함                          //
+                                                                                  //
+            if (dictionary.ContainsKey("mp3"))                                    //
+                Console.WriteLine("mp3 키 값의 데이터가 있음");                   //
+            else                                                                  //
+                Console.WriteLine("mp3 키 값의 데이터가 없음");                   //
+                                                                                  //
+            string output;                                                        //
+            if (dictionary.TryGetValue("bmp", out output))                        //
+                Console.WriteLine(output);                                        //
+            // bmp 키 값이 있을 때 bmp 키 값의 데이터를 출력함                    //
+            else                                                                  //
+                Console.WriteLine("bmp 키 값의 데이터가 없음");                   //
+            // bmp 키 값이 없어서 찾지를 못함
+        }
+
+        static void Main(string[] args)
+        {
+            DataStructure.HashTable<string, string> hashTable = new DataStructure.HashTable<string, string>();
+
+            hashTable.Add("txt", "텍스트 파일");
+            hashTable.Add("bmp", "이미지 파일");
+            hashTable.Add("mp3", "오디오 파일");
+
+            Console.WriteLine(hashTable["txt"]);       // 키값은 인덱서를 통해 접근
+
+            if (hashTable.ContainsKey("mp3"))
+                Console.WriteLine("mp3 키 값의 데이터가 있음");
+            else
+                Console.WriteLine("mp3 키 값의 데이터가 없음");
+
+            if (hashTable.Remove("mp3"))
+                Console.WriteLine("mp3 키 값에 해당하는 데이터를 지움");
+            else
+                Console.WriteLine("mp3 키 값에 해당하는 데이터를 못지움");
+
+            if (hashTable.ContainsKey("mp3"))
+                Console.WriteLine("mp3 키 값의 데이터가 있음");
+            else
+                Console.WriteLine("mp3 키 값의 데이터가 없음");
+
+            string output;
+            if (hashTable.TryGetValue("bmp", out output))
+                Console.WriteLine(output);
+            else
+                Console.WriteLine("bmp 키 값의 데이터가 없음");
+        }
+    }
+}
